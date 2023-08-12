0367d16ce2feb4f22ee25792a5322c9ce4485052
626 <nurjigitumarov22@gmail.com>
2023-08-12 19:23:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        //создание списка, где все элементы должны быть типа Integer
+        ArrayList<Integer> data = new ArrayList<>(Arrays.asList(new Integer[50]));
+        ArrayList<Integer> even = new ArrayList<>();   // чётные
+        ArrayList<Integer> odd = new ArrayList<>();    // нечётные
+
+        //заполнение списка из массива
+        for (Integer san : data) {
+            san = random.nextInt(1, 100);
+            if (san % 2 == 1) {
+                even.add(san);
+            } else {
+                odd.add(san);
+            }
+        }
+        System.out.println("так сандар :");
+        for (Integer a : even) {
+            System.out.print(a + " ");
+        }
+        System.out.println(" ");
+        System.out.println("жуп сандар:");
+        for (Integer s : odd) {
+            System.out.print(s + " ");
+        }
+
+
+    }
+}
