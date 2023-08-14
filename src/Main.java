de1d311d416646ae939dada1f5fd7e8f9f10313d
626 <nurjigitumarov22@gmail.com>
2023-08-14 19:07:44 +0300

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        ArrayList<Integer> data = new ArrayList<>(Arrays.asList(new Integer[50]));
+        ArrayList<Integer> data = new ArrayList<>();
         ArrayList<Integer> even = new ArrayList<>();   // чётные
         ArrayList<Integer> odd = new ArrayList<>();    // нечётные
-
-        for (Integer san : data) {
-            san = random.nextInt(1, 100);
-            if (san % 2 == 1) {
-                even.add(san);
+        for (int i = 0; i <50 ; i++) {
+            data.add(random.nextInt(1,100));
+            if (data.get(i) % 2 == 1) {
+                even.add(data.get(i));
             } else {
-                odd.add(san);
+                odd.add(data.get(i));
             }
         }
         System.out.println("так сандар :");
-        for (Integer a : even) {
-            System.out.print(a + " ");
+        for (Integer  tak: even) {
+            System.out.print(tak + " ");
         }
         System.out.println(" ");
         System.out.println("жуп сандар:");
-        for (Integer s : odd) {
-            System.out.print(s + " ");
+        for (Integer jup : odd) {
+            System.out.print(jup + " ");
         }
 
 
